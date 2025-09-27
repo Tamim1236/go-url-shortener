504d22c80f998df60ce413bcbfae0bb3a03f9783
Tamim1236 <tamim028@hotmail.com>
2025-09-26 22:39:05 -0400

    storage services and testing

--- store/store_service.go
+++ store/store_service.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/go-redis/redis"
+	"github.com/go-redis/redis/v8"
 )
 
 // struct wrapper around raw Redis client
@@ -37,4 +37,22 @@ func InitializeStore() *StorageService {
 	fmt.Printf("\nRedis started successfully: pong message = {%s}", pong)
 	storeService.redisClient = redisClient
 	return storeService
+}
+
+// to save mapping between the originalURL and the generated short URL
+func SaveUrlMapping(shortUrl string, originalUrl string, userId string) {
+	err := storeService.redisClient.Set(ctx, shortUrl, originalUrl, CacheDuration).Err()
+	if err != nil {
+		panic(fmt.Sprintf("Failed saving key url | Error: %v - shortUrl: %s - originalUrl: %s\n", err, shortUrl, originalUrl))
+	}
+}
+
+// retrieve initial long URL when short URL is provided
+// user calls shortlink in the URL, so retrieve long URL and then redirect
+func RetrieveInitialUrl(shortUrl string) string {
+	result, err := storeService.redisClient.Get(ctx, shortUrl).Result()
+	if err != nil {
+		panic(fmt.Sprintf("Failed RetrieveInitialUrl url | Error: %v - shortUrl; %s\n", err, shortUrl))
+	}
+	return result
 }
